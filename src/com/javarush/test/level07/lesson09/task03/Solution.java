d23183ff82e0341de59512fe6a55c1bc1a005f2f
Kolomiets Dmitry <caspik@mail.com>
2014-12-16 19:37:57 +0200

    Solutions

--- src/com/javarush/test/level07/lesson09/task03/Solution.java
+++ src/com/javarush/test/level07/lesson09/task03/Solution.java
@@ -15,7 +15,19 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //Напишите тут ваш код
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("мама");
+        list.add("мыла");
+        list.add("раму");
+
+        for (int i = 1; i < list.size()+1; i++){
+            list.add(i, "именно");
+            i++;
+        }
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
 
     }
 }
